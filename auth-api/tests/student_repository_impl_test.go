6ca971b229a2612126759c32a9b66d91f458aa8e
PyMarcus <marcus-v@outlook.com>
2025-05-18 17:54:51 -0300

    feat: added student repository

---
+++ auth-api/tests/student_repository_impl_test.go
@@ -0,0 +1,23 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/PyMarcus/FreelaIF/auth-api/auth-api/tests/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStudentRepositoryCreate(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mocks.NewMockIStudentRepository(ctrl)
+	_, Student := createUsersToTest()
+
+	mockRepo.EXPECT().Create(Student).Return(nil)
+
+	err := mockRepo.Create(Student)
+	assert.NoError(t, err)
+
+}
