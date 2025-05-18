419b1baf4e431aa7d6788f345819d60665bdaddb
PyMarcus <marcus-v@outlook.com>
2025-05-18 17:58:50 -0300

    fix: DTO

--- auth-api/internal/adapters/dtos/user_request_dto.go
+++ auth-api/internal/adapters/dtos/user_request_dto.go
@@ -1,6 +1,18 @@
 package dtos
 
-type UserRequestDTO struct{
+type UserLoginRequestDTO struct{
+	Email string 
+	Password string
+}
+
+type UserRegisterCustomerRequestDTO struct{
+	// TODO: adicionar outros campos
+	Email string 
+	Password string
+}
+
+type UserRegisterStudentRequestDTO struct{
+	// TODO: adicionar outros campos
 	Email string 
 	Password string
 }
