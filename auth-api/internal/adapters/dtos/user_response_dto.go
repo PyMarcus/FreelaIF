ab9a73a1f0b4fe7057ba5423a3db3a3420d62061
PyMarcus <marcus-v@outlook.com>
2025-05-27 21:00:03 -0300

    feat: dtos

---
+++ auth-api/internal/adapters/dtos/user_response_dto.go
@@ -0,0 +1,36 @@
+package dtos
+
+import "time"
+
+type CustomerResponseDTO struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+	CPF       string    `json:"cpf"`
+	CNPJ      string    `json:"cnpj,omitempty"`
+	Phone     string    `json:"phone"`
+	Address   string    `json:"address"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type StudentResponseDTO struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+	CPF       string    `json:"cpf"`
+	Phone     string    `json:"phone"`
+	Address   string    `json:"address"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type RegisterCustomerResponse struct {
+	Message  string              `json:"message"`
+	Customer CustomerResponseDTO `json:"customer"`
+}
+
+type RegisterStudentResponse struct {
+	Message string             `json:"message"`
+	Student StudentResponseDTO `json:"student"`
+}
