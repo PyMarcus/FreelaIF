907782f203a31a962bfd68a8498ba13a87c0531c
Gessyca <gessycadclm@gmail.com>
2025-07-23 20:32:24 -0300

    fix: ci

--- project/src/main/java/com/freelaif/project/ProjectApplication.java
+++ project/src/main/java/com/freelaif/project/ProjectApplication.java
@@ -2,10 +2,8 @@ package com.freelaif.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
